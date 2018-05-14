56ce535634f8699ecbcee3e35f913120983a7a81
luisro369 <00005513@uca.edu.sv>
2018-05-14 16:21:20 -0600

    Initial commit

---
+++ app/src/main/java/com/luisro00005513/tarealabo6/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.luisro00005513.tarealabo6.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.luisro00005513.tarealabo6.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
